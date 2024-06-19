39018cb2eba34e3a8b98a46c9b3fae31fe9cd451
Raxuis <raclot.raphael@gmail.com>
2024-06-19 13:08:26 +0200

    feat(welcome message): :sparkles: add items array in props

--- src/components/homepage/WelcomeMessage.tsx
+++ src/components/homepage/WelcomeMessage.tsx
@@ -1,18 +1,27 @@
 type WelcomeMessageProps = {
   name?: string,
-  age: number
+  age: number,
+  items?: string[]
 }
 
 const Greeting = (props: WelcomeMessageProps) => {
   return (
-    <div className="mx-auto py-4">Welcome {props.name ?? 'Anonymous'}!</div>
+    <div className="space-y-2">
+      <p>Welcome {props.name ?? 'Anonymous'}!</p>
+      <p>You are {props.age} years old.</p>
+      {props.items && <><p>Here are your items :</p>
+        <ul className="list-disc list-inside">
+          {props.items.map((item, index) => <li key={index}>{item}</li>)}
+        </ul></>}
+    </div>
   )
 }
 
 export const WelcomeMessage = () => {
   return (
-    <div className="my-4 max-w-sm px-4">
-      <Greeting age={9} name="Toto" />
+    <div className="my-4 max-w-sm px-4 space-y-24">
+      <Greeting age={9} name="Toto" items={['Bike', 'Pencil']} />
+      <Greeting age={19} name="Raphaël" />
     </div>
   )
 }
